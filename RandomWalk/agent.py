ab5539929a11ad2f71971530f4d655e23631b97a
Boucher <124088772+rhit-bouchemj@users.noreply.github.com>
2024-09-15 15:37:34 -0400

    animation and sensing complete model

--- RandomWalk/agent.py
+++ RandomWalk/agent.py
@@ -7,17 +7,24 @@ class Agent():
         self.xPos = 0
         self.yPos = 0
         self.direction = 0 #in radians (0 - 2pi)
-        self.velocity = 1
+        self.velocity = 0.5
         self.sourceDistance = 1.0
-        # self.prevSourceDistance = sys.maxint
+        self.prevSourceDistance = 0.0 #sys.maxsize
+        self.probabilityToRotate = 0.5
+        self.sensing = False
+
+    def setSensing(self):
+        self.sensing = True
 
     def calculateSourceDistance(self, sourceX, sourceY):
-        sourceDistance = np.sqrt(((self.xPos - sourceX) ** 2) + ((self.yPos-sourceY) ** 2))
-        return sourceDistance
+        self.sourceDistance = np.sqrt(((self.xPos - sourceX) ** 2) + ((self.yPos-sourceY) ** 2))
+        return self.sourceDistance
     
-    # def movedFarther(self):
-    #     return self.sourceDistance > self.prevSourceDistance
-
+    # def checkMovedFarther(self):
+    #     self.movedFarther = self.sourceDistance > self.prevSourceDistance
+    def movedFarther(self):
+        return self.sourceDistance > self.prevSourceDistance
+        
     def getSourceDistance(self):
         return self.sourceDistance
 
@@ -31,12 +38,21 @@ class Agent():
         self.direction = np.random.random() * 2 * np.pi
 
     def move(self):
-        self.velocity = np.random.random() * 0.5
+        # self.velocity = np.random.random() * 0.5
         self.xPos += self.velocity * np.cos(self.direction)
         self.yPos += self.velocity * np.sin(self.direction)
 
     def step(self):
-        self.rotate()
+        # if(np.random.random() = self.probabilityToRotate):
+        if(self.sensing):
+            # self.checkMovedFarther()
+            if(self.movedFarther() or (np.random.random() < self.probabilityToRotate)): #add random noise
+                self.rotate()
+            # else:
+                # print("notTurn")
+            self.prevSourceDistance = self.sourceDistance
+        else:
+            self.rotate()
         self.move()
 
     def getOriginDistance(self):
--- RandomWalk/animationTest.py
+++ RandomWalk/animationTest.py
@@ -6,47 +6,58 @@ import agent
 
 
 def main():
-        loopIterations = 1000
-        pop = 60
+        loopIterations = 100
+        pop = 50
+        sensing = True
 
-        #Create a stimulus source at 
-        sourceX = 30
-        sourceY = 20
+        #Create a stimulus source at X,Y
+        source = [80, -20]
 
-        (xPosList, yPosList) = multipleOrganismSimulation(pop, loopIterations)
+        (xPosList, yPosList, distanceList) = multipleOrganismSimulation(pop, loopIterations, sensing, source)
         print(xPosList)
         print(yPosList)
+        print(distanceList)
+        print(np.sum(xPosList.T[-1]))
+        print(np.sum(yPosList.T[-1]))
+        plotDistance(loopIterations, distanceList, pop)
         animate_plot(loopIterations, xPosList, yPosList, pop)
     
         
-def singleOrganismSimulation(iterations):
+def singleOrganismSimulation(iterations, sensing, source):
     loopIterations = iterations
     organism = agent.Agent()
+    if(sensing):
+        organism.setSensing()
     xPosList = np.zeros(iterations)
     yPosList = np.zeros(iterations)
+    distance = np.zeros(iterations)
     for i in range(1,iterations):
+        organism.calculateSourceDistance(source[0], source[1])
         organism.step()
         xPosList[i] = organism.getX()
         yPosList[i] = organism.getY()
-    return xPosList, yPosList
+        distance[i] = organism.getOriginDistance()
+    return xPosList, yPosList, distance
 
 
-def multipleOrganismSimulation(population, iterations):
+def multipleOrganismSimulation(population, iterations, sensing=False, source=[0,0]):
     xPosList = np.zeros((population, iterations))
     yPosList = np.zeros((population, iterations))
+    distance = np.zeros((population, iterations))
     for numOrganism in range(population):
-        currentXList, currentYList = singleOrganismSimulation(iterations)
+        currentXList, currentYList, currentDistance = singleOrganismSimulation(iterations, sensing, source)
         xPosList[numOrganism] = currentXList
         yPosList[numOrganism] = currentYList
-    return xPosList, yPosList
+        distance[numOrganism] = currentDistance
+    return xPosList, yPosList, distance
 
-def animate_plot(iteration, xList, yList, population=1):
-    fig, ax = plt.subplots()
+def animate_plot(iteration, xList, yList, population=1, frameModifer=1):
+    fig1, ax1 = plt.subplots()
     x_data = [[] for numLine in range(population)]
     y_data = [[] for numLine in range(population)]
-    lines = [ax.plot([], [], animated=True)[0] for lineNum in range(population)]
+    lines = [ax1.plot([], [], animated=True)[0] for lineNum in range(population)]
     origin_dot, = plt.plot(0, 0, 'ko', markersize=3)  # 'ko' means black color ('k') and circle marker ('o')
-    end_dots, = plt.plot(xList.T[-1], yList.T[-1], 'mo', marker='2')
+    end_dots, = plt.plot(xList.T[-1], yList.T[-1], 'ko', marker='2')
 
     # colors = plt.cm.viridis(np.linspace(0, 1, population))  # Generate a range of colors for lines
     colors = plt.cm.hsv(np.linspace(0, 1, population))  # 'hsv' covers the full spectrum of hues
@@ -55,19 +66,34 @@ def animate_plot(iteration, xList, yList, population=1):
         line.set_color(color)
 
     def init():
-        ax.set_xlim(-50, 50)
-        ax.set_ylim(-50, 50)
+        xLim = 100
+        yLim = 50
+        ax1.set_xlim(-xLim, xLim)
+        ax1.set_ylim(-yLim, yLim)
         return lines + [origin_dot] + [end_dots]
     
-    def update(frame, xList, yList):
+    def update(frame, xList, yList, frameModifer):
         for numLine in range(population):
-            x_data[numLine].append(xList[numLine][frame])
-            y_data[numLine].append(yList[numLine][frame])
+            x_data[numLine] = (xList[numLine][:frame*frameModifer])
+            y_data[numLine] = (yList[numLine][:frame*frameModifer])
             lines[numLine].set_data(x_data[numLine], y_data[numLine])
         return lines + [origin_dot] + [end_dots]
 
-    ani = FuncAnimation(fig, update, fargs=(xList, yList, ), frames=iteration, interval=1, init_func=init, blit=True, repeat=False)
+    ani = FuncAnimation(fig1, update, fargs=(xList, yList, frameModifer, ), frames=iteration+1, interval=1, init_func=init, blit=True, repeat=False)
     # plt.plot( 0,0,'ko')
     plt.show()  # Display the plot
+
+def plotDistance(iterations, distances, population):
+    fig2, ax2 = plt.subplots()
+    ax2.set_xlabel("Step Number")
+    ax2.set_ylabel("Distane from Origin")
+    stepNum = range(0, iterations)
+    colors = plt.cm.hsv(np.linspace(0, 1, population))  # 'hsv' covers the full spectrum of hues
+    avgDistance = np.zeros(iterations)
+    for i in range(iterations):
+        avgDistance[i] = np.average(distances.T[i])
+    for i in range(population):
+        ax2.plot(stepNum, distances[i], color=colors[i], label=f'Line {i+1}')  # Assigning different colors and labels
+    ax2.plot(stepNum, avgDistance, 'k')
 
 main()
